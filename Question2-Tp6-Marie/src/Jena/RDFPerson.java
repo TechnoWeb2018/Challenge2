a4be24f244e39e40cabb98d6badef06644895b3b
TechnoWeb2018 <38584059+TechnoWeb2018@users.noreply.github.com>
2018-04-23 10:20:18 +0200

    Question2-Tp5-Marie

---
+++ Question2-Tp6-Marie/src/Jena/RDFPerson.java
@@ -0,0 +1,78 @@
+package Jena;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.sparql.vocabulary.FOAF;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.rdf.model.Statement;
+
+public class RDFPerson {
+	
+	private Model model;
+	
+	public void writeModel() {
+		model.write(System.out,"RDF/XML");
+		model.write(System.out, "TURTLE");
+	}
+	
+	public void addStatement(String s, String p, String o) {
+		Resource subject = model.createResource(s);
+		Property predicate = model.createProperty(p);
+		RDFNode object = model.createResource(o);
+		Statement statement = model.createStatement(subject, predicate, object);
+		model.add(statement);
+	}
+	
+	public void addStatement(String s, Property p, String o) {
+		Resource subject = model.createResource(s);
+		RDFNode object = model.createResource(o);
+		Statement statement = model.createStatement(subject, p, object);
+		model.add(statement);
+	}
+	
+	public void createModel() {
+		model = ModelFactory.createDefaultModel();
+		String ns ="http://www.example.org#";
+		
+		//Set a namespace
+		model.setNsPrefix("per", ns);
+		model.setNsPrefix("foaf", FOAF.getURI());
+		
+		addStatement(ns + "Student",RDFS.subClassOf, ns + "Person");
+		addStatement(ns + "Staff",RDFS.subClassOf, ns + "Person");
+		
+		addStatement(ns + "Undergraduate",RDFS.subClassOf, ns + "Student");
+		addStatement(ns + "Graduate",RDFS.subClassOf, ns + "Student");
+		
+		addStatement(ns + "Professor",RDFS.subClassOf, ns + "Staff");
+		addStatement(ns + "Assistant",RDFS.subClassOf, ns + "Staff");
+		
+		// addStatement(ns + "prof1", RDF.type, ns + "Professor");
+		// addStatement(ns + "prof1", FOAF.name, ns + "Bob");
+		// addStatement(ns + "prof1", FOAF.age, ns + "50");
+		
+		RDFNode undergraduate = model.createResource(ns + "Undergraduate");
+		RDFNode professor = model.createResource(ns + "Professor");
+		
+		Resource alice = model.createResource()
+								.addProperty(RDF.type, undergraduate)
+								.addLiteral(FOAF.name, "Alice")
+								.addLiteral(FOAF.age, 36);
+		
+		Resource bob = model.createResource()
+				.addProperty(RDF.type, professor)
+				.addLiteral(FOAF.name, "Bob")
+				.addLiteral(FOAF.age, 50);
+	}
+
+	public static void main(String[] args) {
+		RDFPerson person = new RDFPerson();
+		person.createModel();
+		person.writeModel();
+	}
+
+}
